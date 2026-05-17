e3d81757a2f42a70f03b89446c56050739239447
jb <jb@foo.bar>
2026-05-17 03:18:42 +0200

    test: add test files for LSP validation
    - foo.py (Python)
    - index.ts (TypeScript)
    - main.c, main.cpp (C/C++)
    - main.cs (C#)
    - foo.json, foo.toml, foo.css (config files)
    - test.bash (Bash)
    - test.md, test.tex, test.xml (markup)
    - nginx.conf, Dockerfile, docker-compose.yaml (devops)

---
+++ testfiles/foo.py
@@ -0,0 +1,33 @@
+import os
+
+
+
+# aaaaaaa
+# bbbbb
+# ccccc
+
+# aabbccddeeff
+# -> FOO
+
+
+# aaaaaaa
+# bbbbb
+# ccccc
+
+
+def foo():
+    print("BLA")
+
+
+foo()
+foo()
+foo()
+foo()
+
+# ccccc
+god = 1
+bla2 = "fooo"
+bla2 = "fooo"
+print(bla2)
+print(bla2)
+print("")

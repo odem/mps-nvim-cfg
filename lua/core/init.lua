3589a19fa16530c04983f1f92a15d5568699c17e
jb <jb@foo.bar>
2026-05-17 04:13:24 +0200

    refactor: create core/init.lua aggregator, simplify plugins.lua

---
+++ lua/core/init.lua
@@ -0,0 +1,15 @@
+return {
+	require("core.mason"),
+	require("core.autopairs"),
+	require("core.fidget"),
+	require("core.completion"),
+	require("core.luasnip"),
+	require("core.friendly-snippets"),
+	require("core.treesitter"),
+	require("core.lazydev"),
+	require("core.neoconf"),
+	require("core.undotree"),
+	require("core.telescope"),
+	require("core.telescope-custom"),
+	require("core.which-key"),
+}
--- lua/init/plugins.lua
+++ lua/init/plugins.lua
@@ -1,17 +1,6 @@
 require("lazy").setup({
 	spec = {
-		require("core.mason"),
-		require("core.autopairs"),
-		require("core.fidget"),
-		require("core.completion"),
-		require("core.luasnip"),
-		require("core.friendly-snippets"),
-		require("core.treesitter"),
-		require("core.lazydev"),
-		require("core.neoconf"),
-		require("core.undotree"),
-		require("core.telescope"),
-		require("core.which-key"),
+		require("core"),
 		require("appearance"),
 		require("navigation"),
 		require("tools"),

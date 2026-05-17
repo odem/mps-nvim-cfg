f66c34b32a3cc8c0845d586abf498451ecbff352
jb <jb@foo.bar>
2026-05-17 02:48:37 +0200

    feat(lsp): add 18 new LSP servers from old config
    - html, cssls, ts_ls, dockerls, docker_compose, jsonls, marksman
    - texlab, lemminx, bashls, rust_analyzer, taplo, vimls
    - nginx_language_server, cmake, clangd, csharp_ls, ansiblels
    - Auto-install via Mason on startup

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -11,9 +11,34 @@ vim.lsp.config["*"] = {
 -- Auto-install missing servers via Mason (deferred, non-blocking)
 vim.schedule(function()
 	local servers = {
+		-- Python
 		{ bin = "ruff", mason = "ruff" },
 		{ bin = "pyright-langserver", mason = "pyright" },
+		-- Lua
 		{ bin = "lua-language-server", mason = "lua-language-server" },
+		-- Web
+		{ bin = "html", mason = "html" },
+		{ bin = "css-lsp", mason = "css-lsp" },
+		{ bin = "typescript-language-server", mason = "typescript-language-server" },
+		-- Docker
+		{ bin = "dockerfile-language-server", mason = "dockerfile-language-server" },
+		{ bin = "docker-compose-language-service", mason = "docker-compose-language-service" },
+		-- Config
+		{ bin = "json-lsp", mason = "json-lsp" },
+		{ bin = "taplo", mason = "taplo" },
+		-- Markup
+		{ bin = "marksman", mason = "marksman" },
+		{ bin = "texlab", mason = "texlab" },
+		{ bin = "lemminx", mason = "lemminx" },
+		-- Shell
+		{ bin = "bash-language-server", mason = "bash-language-server" },
+		-- Systems
+		{ bin = "rust-analyzer", mason = "rust-analyzer" },
+		{ bin = "clangd", mason = "clangd" },
+		{ bin = "cmake-language-server", mason = "cmake-language-server" },
+		{ bin = "vim-language-server", mason = "vim-language-server" },
+		{ bin = "nginx-language-server", mason = "nginx-language-server" },
+		{ bin = "csharp-ls", mason = "csharp-ls" },
 	}
 	for _, s in ipairs(servers) do
 		if vim.fn.executable(s.bin) ~= 1 then
@@ -73,6 +98,203 @@ vim.lsp.config.lua_ls = {
 }
 
 vim.lsp.enable("lua_ls")
+
+-- HTML
+vim.lsp.config.html = {
+	cmd = { "html", "--stdio" },
+	filetypes = { "html", "templ" },
+	root_markers = { "package.json", ".git" },
+}
+vim.lsp.enable("html")
+
+-- CSS
+vim.lsp.config.cssls = {
+	cmd = { "css-lsp", "--stdio" },
+	filetypes = { "css", "scss", "less" },
+	root_markers = { "package.json", ".git" },
+	settings = {
+		css = { validate = true },
+		scss = { validate = true },
+		less = { validate = true },
+	},
+}
+vim.lsp.enable("cssls")
+
+-- TypeScript/JavaScript
+vim.lsp.config.ts_ls = {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = { "typescript", "javascript" },
+	root_markers = { "package.json", "tsconfig.json", ".git" },
+}
+vim.lsp.enable("ts_ls")
+
+-- Docker
+vim.lsp.config.dockerls = {
+	cmd = { "dockerfile-language-server", "--stdio" },
+	filetypes = { "dockerfile" },
+	root_markers = { "Dockerfile", ".git" },
+}
+vim.lsp.enable("dockerls")
+
+-- Docker Compose
+vim.lsp.config.docker_compose_language_service = {
+	cmd = { "docker-compose-language-service", "stdio" },
+	filetypes = { "yaml.docker-compose" },
+	root_markers = {
+		"docker-compose.yaml",
+		"docker-compose.yml",
+		"compose.yaml",
+		"compose.yml",
+	},
+}
+vim.lsp.enable("docker_compose_language_service")
+
+-- JSON
+vim.lsp.config.jsonls = {
+	cmd = { "json-lsp", "--stdio" },
+	filetypes = { "json", "jsonc" },
+	root_markers = { ".git" },
+}
+vim.lsp.enable("jsonls")
+
+-- Markdown
+vim.lsp.config.marksman = {
+	cmd = { "marksman", "server" },
+	filetypes = { "markdown", "markdown.mdx" },
+	root_markers = { ".git", ".marksman.toml" },
+}
+vim.lsp.enable("marksman")
+
+-- LaTeX
+vim.lsp.config.texlab = {
+	cmd = { "texlab" },
+	filetypes = { "tex", "plaintex", "bib" },
+	settings = {
+		texlab = {
+			build = {
+				args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+				executable = "latexmk",
+			},
+			chktex = { onEdit = false, onOpenAndSave = false },
+			diagnosticsDelay = 300,
+			formatterLineLength = 80,
+		},
+	},
+}
+vim.lsp.enable("texlab")
+
+-- XML
+vim.lsp.config.lemminx = {
+	cmd = { "lemminx" },
+	filetypes = { "xml", "xsd", "xsl", "xslt", "svg" },
+	root_markers = { ".git" },
+}
+vim.lsp.enable("lemminx")
+
+-- Bash
+vim.lsp.config.bashls = {
+	cmd = { "bash-language-server", "start" },
+	filetypes = { "sh", "bash" },
+	root_markers = { ".git" },
+	settings = {
+		bashIde = {
+			shellcheckPath = "shellcheck",
+			shellcheckArguments = "--color=always -S style -W 1 -x",
+		},
+	},
+}
+vim.lsp.enable("bashls")
+
+-- Rust
+vim.lsp.config.rust_analyzer = {
+	cmd = { "rust-analyzer" },
+	filetypes = { "rust" },
+	root_markers = { "Cargo.toml", ".git" },
+	settings = {
+		["rust-analyzer"] = {},
+	},
+}
+vim.lsp.enable("rust_analyzer")
+
+-- TOML
+vim.lsp.config.taplo = {
+	cmd = { "taplo", "lsp", "stdio" },
+	filetypes = { "toml" },
+	root_markers = { ".toml", ".git" },
+}
+vim.lsp.enable("taplo")
+
+-- Vim
+vim.lsp.config.vimls = {
+	cmd = { "vim-language-server", "--stdio" },
+	filetypes = { "vim" },
+	root_markers = { ".git" },
+	settings = {
+		diagnostic = { enable = true },
+		indexes = { count = 3, gap = 100 },
+		isNeovim = true,
+	},
+}
+vim.lsp.enable("vimls")
+
+-- Nginx
+vim.lsp.config.nginx_language_server = {
+	cmd = { "nginx-language-server", "--stdio" },
+	filetypes = { "nginx" },
+	root_markers = { ".git" },
+}
+vim.lsp.enable("nginx_language_server")
+
+-- CMake
+vim.lsp.config.cmake = {
+	cmd = { "cmake-language-server" },
+	filetypes = { "cmake" },
+	root_markers = { "CMakeLists.txt", "CMakePresets.json", ".git" },
+	settings = {
+		buildDirectory = "build",
+	},
+}
+vim.lsp.enable("cmake")
+
+-- C/C++
+vim.lsp.config.clangd = {
+	cmd = { "clangd", "--offset-encoding=utf-16" },
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+	root_markers = {
+		".clangd",
+		".clang-tidy",
+		"compile_commands.json",
+		"compile_flags.txt",
+		".git",
+	},
+}
+vim.lsp.enable("clangd")
+
+-- C#
+vim.lsp.config.csharp_ls = {
+	cmd = { "csharp-language-server" },
+	filetypes = { "cs" },
+	root_markers = { ".csproj", ".git" },
+	settings = {
+		AutomaticWorkspaceInit = true,
+	},
+}
+vim.lsp.enable("csharp_ls")
+
+-- Ansible
+vim.lsp.config.ansiblels = {
+	cmd = { "ansible-language-server", "--stdio" },
+	filetypes = { "yaml", "yml" },
+	root_markers = { ".ansible-lint", ".git" },
+	settings = {
+		ansible = {
+			ansible = { path = "ansible", useFullyQualifiedCollectionNames = true },
+			ansibleLint = { enabled = true, path = "ansible-lint" },
+			completion = { provideRedirectModules = true },
+		},
+	},
+}
+vim.lsp.enable("ansiblels")
 
 -- Inlay hints
 vim.lsp.inlay_hint.enable(true)
